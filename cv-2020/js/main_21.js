170cf74d2fd02592f6cf5c82d3e70e54d7c0a380
ljubisas <ljubisa.s@gmail.com>
2021-02-27 18:23:43 +0100

    dynamic version

--- cv-2020/js/main_21.js
+++ cv-2020/js/main_21.js
@@ -6,8 +6,6 @@ let layerBehind = starsContainer.querySelector(".behind");
 let layerMiddle = starsContainer.querySelector(".middle");
 let layerCloseup = starsContainer.querySelector(".closeup");
 */
-
-
 class Panel {
     constructor(panelName, panelID, panelHolder, buttonsList) {
         let bottomResizerEnabled = true;
@@ -87,9 +85,8 @@ class Panel {
                     
                     console.log("close clocked", panelAnimation);
                     const panel = this.parentElement.parentElement.parentElement;
-                    panel.classList.add('removing');
-                    setTimeout(function(){ panel.remove()}, 300);
-                    panelController.removePanel(1);
+                    //panel.classList.add('removing');
+                    //setTimeout(function(){ panel.remove()}, 300);
                     //removePanel
                 }); 
                 break;
@@ -175,8 +172,7 @@ class PanelControler {
     constructor(panelsWrapperControler) {
         let panelCounter = 0;
         console.log("PanelControler",panelsWrapperControler);
-        this.panelList = [];
-        let defaultButtons = ['minimize', 'more', 'close'];
+        let panelList = [];
         //let newPanel11 = new Panel('TETET', 'p11', '.panels-wrapper',['minimize', 'more', 'close']);
         
         this.wrapperInput = document.querySelector("#wrapperInput");
@@ -187,22 +183,124 @@ class PanelControler {
                 panelCounter++;
                 console.log( event.currentTarget.value);
                 
-                let newPanel = new Panel(event.currentTarget.value, `p${panelCounter}`, panelsWrapperControler, defaultButtons);
-                this.panelList.push(newPanel);
-                console.log(this.panelList);
+                let newPanel = new Panel(event.currentTarget.value, `p${panelCounter}`, panelsWrapperControler,['minimize', 'more', 'close']);
+                panelList.push(newPanel);
+                console.log(panelList);
             }
         })
     }
     removePanel(panelID = '1') {
-        console.log('removePanel', panelID, this.panelList);
+        console.log('removePanel', panelID, panelList);
 
     }
 }
 
 
+
+
+  const getRepaintInterval = () => {
+    return new Promise((resolve) => {
+        requestAnimationFrame((t1) => {
+        requestAnimationFrame((t2) => {
+            resolve(t2 - t1);
+        });
+        });
+    });
+};
+const getFps = () => new Promise(resolve => {
+    let repaint = 0;
+    const start = performance.now();
+    const withRepaint = () => {
+        requestAnimationFrame(() => {
+            if ((performance.now() - start) < 1000) {
+                repaint += 1;
+                withRepaint();
+            } else {
+                resolve(repaint);
+            }
+        });
+    };
+    withRepaint();
+});
+
+const fpsHandler = () => {
+    let repaint;
+    let stop;
+    let ret;
+    let start;
+    const init = () => {
+        ret = undefined;
+        stop = false;
+        repaint = 0;
+        start = performance.now();
+    };
+    init();
+    const withRepaint = () => {
+        requestAnimationFrame(() => {
+            if (!stop) {
+                repaint += 1;
+                withRepaint();
+            }
+        });
+    };
+    return {
+        start: () => {
+            init();
+            withRepaint();
+        },
+        end: () => {
+            stop = true;
+            if (!ret) ret = repaint / ((performance.now() - start) / 1000);
+            return ret;
+        }
+    }
+};
+
+const { start, end } = fpsHandler();
+
+function addStars(){
+    let maxStars = 30;
+    let starEl = starsContainer.querySelector('.star');
+    let cln; // = starEl.cloneNode(true);
+
+    
+
+    for (let i =0; i<maxStars; i++) {
+        let step = getRandomInt(100);    
+        let layer;
+        console.log(step);
+        
+        if (step < 40) layer = starsContainer.querySelector('.behind');
+        if (step > 40 && step < 70) layer = starsContainer.querySelector('.middle');                
+        if (step > 70) layer = starsContainer.querySelector('.closeup');
+        
+
+        cln = starEl.cloneNode(true);
+        resizeElement(cln, step);
+        randomPositionElement(cln);
+        //setTimeout(function(_layer){ _layer.appendChild(cln); }, 300, layer);
+        layer.appendChild(cln); 
+    }
+    
+}
+function randomPositionElement(el){
+    el.style.left=getRandomInt(100)+"%";
+    el.style.top=getRandomInt(100)+"%";
+};
+function resizeElement(el, step = 80){    
+    let svg = el.querySelector('svg');
+    console.log("resizeElement", step);
+    el.style.opacity = step/100;
+    svg.style.width = step+"px";
+    svg.style.height = step+"px";
+    svg.style.border = "1px solid #ccc";
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
 var intervalID;
 let panelController = new PanelControler(".panels-wrapper");
-//panelController.addNewPanel('panel1', ['add', 'remove', 'minimize', 'close']);
 //let newPanel1 = new Panel('panel1', 'p1', '.panels-wrapper',['add', 'remove', 'minimize', 'close']);
 
 
@@ -213,7 +311,7 @@ window.onload = function() {
   };
 
 function init(){
-    //console.log("doc ready", getRepaintInterval);
+    console.log("doc ready", getRepaintInterval);
     //here code
     //addSettingsInfo("#settings", "TEST Set", 'ready');
     //addStars();
