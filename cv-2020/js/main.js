661a9a564e87e852562193c71c660012c2f2fb8f
ljubisas <ljubisa.s@gmail.com>
2021-02-27 20:53:49 +0100

    working panels

--- cv-2020/js/main.js
+++ cv-2020/js/main.js
@@ -11,6 +11,7 @@ let layerCloseup = starsContainer.querySelector(".closeup");
 class Panel {
     constructor(panelName, panelID, panelHolder, buttonsList) {
         let bottomResizerEnabled = true;
+        this.panelID = panelID;
         this.enablePanelAnim = true;
         this.panelHolderElement = document.querySelector(panelHolder);
         this._panelName = panelName;
@@ -18,16 +19,17 @@ class Panel {
         this.newPanelHeader = document.createElement("header");
         this.newPanelHeaderLabel = document.createElement("div");
         this.newPanelInfo = document.createElement("div");
-        
         const newPanelTitle = document.createTextNode(panelName);
     
         this.newPanel.classList.add("panel", panelID);
+        this.newPanel.id = panelID;
         this.newPanelInfo.classList.add("info");
         this.newPanelHeaderLabel.classList.add('label');
         this.newPanelHeaderLabel.appendChild(newPanelTitle);
     
         this.newPanelHeader.appendChild(this.newPanelHeaderLabel);
         this.newPanel.appendChild(this.newPanelHeader);
+        //this.newPanel.appendChild(this.wrapperInput);
         this.newPanel.appendChild(this.newPanelInfo);
 
         if (!bottomResizerEnabled) {
@@ -44,7 +46,7 @@ class Panel {
 
     initElements() {
         console.log('init');
-        this.panelHolderElement = document.querySelector(panelHolder);
+        this.panelHolderElement = panelController.panelHolderElement; //document.querySelector(panelHolder);
         this._panelName = panelName;
         this.newPanel = document.createElement("div");
         this.newPanelHeader = document.createElement("header");
@@ -74,6 +76,12 @@ class Panel {
     addBottomResizer(){
         this.addTextarea('panels-wrapper__resizer', '')
     }
+    removePanelFunction(){
+        console.log('removePanelFunction');
+        const panel = this.newPanel; //this.parentElement.parentElement.parentElement;
+        panel.classList.add('removing');
+        setTimeout(function(){ panel.remove()}, 300);
+    }
     addPanelHeaderButton(btnType, buttonWrapper) {        
         let panelButton = document.createElement("button");
         let panelAnimation = this.enablePanelAnim;
@@ -87,10 +95,11 @@ class Panel {
                     
                     console.log("close clocked", panelAnimation);
                     const panel = this.parentElement.parentElement.parentElement;
-                    panel.classList.add('removing');
-                    setTimeout(function(){ panel.remove()}, 300);
-                    panelController.removePanel(1);
-                    //removePanel
+                    
+                    //panel.classList.add('removing');                    
+                    //setTimeout(function(){ panel.remove()}, 300);            
+                    
+                    panelController.removePanel(panel.id);     
                 }); 
                 break;
             case 'minimize':
@@ -168,40 +177,62 @@ class Panel {
         let statValue = items[Number(itemRow)].querySelector('.stat-value');
         statValue.innerHTML = value;
     }
+    destroy(){
+        
+    }
 }
 
 
-class PanelControler {
-    constructor(panelsWrapperControler) {
+class PanelController {
+    constructor(_panelsWrapperControler) {
         let panelCounter = 0;
-        console.log("PanelControler",panelsWrapperControler);
-        this.panelList = [];
-        let defaultButtons = ['minimize', 'more', 'close'];
-        //let newPanel11 = new Panel('TETET', 'p11', '.panels-wrapper',['minimize', 'more', 'close']);
-        
-        this.wrapperInput = document.querySelector("#wrapperInput");
+        let panelsWrapperControler = _panelsWrapperControler;
+        let panelHolderElement = document.querySelector(panelsWrapperControler);
 
+        const panelCounterPrefix = 'p-item-';        
+        const defaultButtons = ['minimize', 'more', 'close'];       
+
+        this.panelList = [];   
+        
+        this.wrapperInput = document.createElement("input");
+        this.wrapperInput.id = 'wrapperInput';
+
+        panelHolderElement.insertBefore(this.wrapperInput, panelHolderElement.querySelector('.panels-wrapper__resizer'));
+     
+
+        console.log("PanelControler constructor: ", panelsWrapperControler);
+        //this.wrapperInput = document.querySelector("#wrapperInput");
         this.wrapperInput.addEventListener("keyup", event =>{
-            //console.log("input changed", event);
             if (event.keyCode === 13) {
-                panelCounter++;
-                console.log( event.currentTarget.value);
-                
-                let newPanel = new Panel(event.currentTarget.value, `p${panelCounter}`, panelsWrapperControler, defaultButtons);
-                this.panelList.push(newPanel);
-                console.log(this.panelList);
+                this.addNewPanel(event.currentTarget.value, panelCounterPrefix + panelCounter, panelsWrapperControler, defaultButtons)
+                panelCounter++;                
             }
         })
     }
-    removePanel(panelID = '1') {
-        console.log('removePanel', panelID, this.panelList);
+    addNewPanel(value, panelID, panelsWrapperControler, defaultButtons){
+        let newPanel = new Panel(value, panelID, panelsWrapperControler, defaultButtons);
+        this.panelList.push(newPanel);
+    }
+    removePanel(panelID) {
+        console.log('-- PanelController removePanel');
+        let counter = 0;
+        let panelIndex = 0;
+        this.panelList.forEach(element => {
+            if (element.newPanel.id === panelID) {                
+                console.log('MATCH', element.newPanel.id,'=', panelID,'/', counter);
+                panelIndex = counter;
+                element.removePanelFunction();
+            }                           
+            counter++;
+        });
+        this.panelList = this.panelList.slice(0, panelIndex).concat(this.panelList.slice(panelIndex + 1, this.panelList.length));
 
     }
 }
 
 
 var intervalID;
-let panelController = new PanelControler(".panels-wrapper");
+let panelController = new PanelController(".panels-wrapper");
 //panelController.addNewPanel('panel1', ['add', 'remove', 'minimize', 'close']);
 //let newPanel1 = new Panel('panel1', 'p1', '.panels-wrapper',['add', 'remove', 'minimize', 'close']);
 
@@ -220,15 +251,16 @@ function init(){
     document.querySelector("#start").addEventListener('click', (event) => {
         console.log("clicked");
     });
-
-    /*
-    newPanel1.init();
+    let newPanel1 = new Panel('panel1', 'p1', '.panels-wrapper',['add', 'remove', 'minimize', 'close']);
+    let newPanel2 = new Panel('panel2', 'p2', '.panels-wrapper',['add', 'remove', 'minimize', 'close']);
+    
+    //newPanel1.init();
     newPanel1.addSettingsInfo([["cookieEnabled", navigator.cookieEnabled], ["appCodeName", navigator.appCodeName], ["product", navigator.product]]);
     newPanel1.addTextarea('console', navigator.userAgent)
     newPanel2.addSettingsInfo([["typeof(Worker)", typeof(Worker)], ["s333ssss info", "value"], ["sss44ss info", "value"]]);
     newPanel2.updatePanelItem(2, 400);
     //initTicker();
-    */
+    /**/
     console.log(navigator.userAgent);
 }
 
